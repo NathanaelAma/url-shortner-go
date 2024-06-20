75ae14086fe0f2cc28877d21de72679afefbc7bc
Nathanael Ama <nathanael.ama1@gmail.com>
2024-06-20 11:46:36 +0200

    test(route): add health check route test

---
+++ main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/NathanaelAma/url-shortener/server"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHealthRoute(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
+	router := server.SetupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/health", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "application/json; charset=utf-8", w.Header().Get("Content-Type"))
+
+	var response map[string]string
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	assert.Nil(t, err)
+	assert.Equal(t, "UP", response["status"])
+}
